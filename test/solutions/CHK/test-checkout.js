70870798783ab2fdedfd01f8185ee5a995fadb0e
sbx_user1051 <sbx_user1051@169.254.28.215>
2025-02-16 15:32:25 +0000

    Sun Feb 16 15:32:25 UTC 2025

--- test/solutions/CHK/test-checkout.js
+++ test/solutions/CHK/test-checkout.js
@@ -2,7 +2,7 @@ var mocha = require('mocha')
 var describe = mocha.describe
 var it = mocha.it
 var assert = require('assert');
-const sum = require('../../../lib/solutions/SUM/sum');
+const sum = require('../../../lib/solutions/CHK/checkout');
 
 describe('SUM challenge: adding two numbers', function() {
 	it('should return 3, which is the sum of 1 and 2', function() {
